58e071e98309fe6d29260e150548f1bbbe90fb43
Aaron Te <aaronjte@gmail.com>
2018-05-29 22:52:22 -0400

    fix: add identifiers between server/browser rendered html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+declare let window: any;
+
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  renderer = '';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: string) {}
+
+  ngOnInit() {
+    this.renderer = isPlatformBrowser(this.platformId) ? 'Browser' : 'Server';
+  }
+
+  openAlert() {
+    if (isPlatformBrowser(this.platformId)) {
+      window.alert('Yes it is!');
+    }
+  }
 }
